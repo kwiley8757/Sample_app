d6fee6d89b0b1de79104a06a3ec92db799c94b54
Kate <kwiley8757@gmail.com>
2012-01-16 13:42:29 +0800

    Removed defualt Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  root :to => 'pages#home'
   
-  get "pages/about"
-  
-  get "pages/help"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
