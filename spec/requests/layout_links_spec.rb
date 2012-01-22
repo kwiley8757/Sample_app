ca95bbcd5e4ec746e48ce48ca5c00ca81da9ef6a
Kate <kwiley8757@gmail.com>
2012-01-22 21:48:33 +0800

    Added user sign in

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -22,5 +22,10 @@ describe "LayoutLinks" do
     get '/about'
     response.should have_selector('title', :content => "About")
   end
+  
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
 end
 
